f08a07b9afb10a5546c2dfbd2c3c176666fc2e0b
joelmir <https://github.com/joelmirmanoel/pay-friends.git>
2024-12-11 01:55:28 -0300

    criação do input e button do content

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,8 @@
-import { Container, Header, Content, Title } from './styled';
+import { Container, Header, Content, Title, Box } from './styled';
 import logoWhite from '../../assets/PayFriendsWhite.svg';
-import { Image, Button } from '../../components';
+import Vector from '../../assets/Vector.svg';
+import Pago from '../../assets/Pago.svg';
+import { Image, Button, Input } from '../../components';
 
 export function Home() {
   return (
@@ -12,6 +14,34 @@ export function Home() {
         <Title>Meus pagamentos</Title>
         <Button style={{ background: '#4079C0', paddingLeft: 50, paddingRight: 50 }}>ADICIONAR PAGAMENTO</Button>
       </Content>
+      <Box>
+        <Input isError={false} style={{ width: 333 }} placeholder="Pesquisar por usuário" />
+        <Button style={{ marginLeft: 19 }}>
+          {' '}
+          <Image src={Vector} alt="PayFriends" width={20} height={18} /> Filtrar
+        </Button>
+        <table>
+          <thead>
+            <tr>
+              <th>Usuário</th>
+              <th>Título</th>
+              <th>Data</th>
+              <th>Valor</th>
+              <th>Pago</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <td>Claudia</td>
+            <td>Professor 1</td>
+            <td>23 Jun 2020</td>
+            <td>R$ 100,00</td>
+            <td>
+              <Image src={Pago} alt="PayFriends" width={16} height={11} />
+            </td>
+          </tbody>
+        </table>
+      </Box>
     </Container>
   );
 }
--- src/pages/home/styled.ts
+++ src/pages/home/styled.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
+  background: #ededed;
 `;
 
 export const Title = styled.h1`
@@ -18,7 +19,49 @@ export const Header = styled.div`
 export const Content = styled.div`
   display: flex;
   justify-content: space-between;
-  padding: 102px 88px;
+  padding-top: 102px;
+  padding-right: 88px;
+  padding-left: 80px;
+  padding-bottom: 80px;
+  background: #ededed;
 `;
 
-export const Box = styled.div``;
+export const Box = styled.div`
+  //width: ;
+  //display: flex;
+  background: #ffffff;
+  justify-content: space-between;
+  margin-left: 80px;
+  margin-right: 80px;
+  padding-top: 27px;
+  border-radius: 10px;
+
+  table {
+    width: 100%;
+    padding-top: 30px;
+    padding-bottom: 21px;
+
+    th {
+      font-family: 'Roboto';
+      font-size: 24px;
+      font-weight: 400;
+      text-align: left;
+    }
+  }
+
+  tbody {
+    //width: 100%;
+    //padding-top: 30px;
+    //padding-bottom: 21px;
+
+    td {
+      font-family: 'Roboto';
+      font-size: 20px;
+      font-weight: 400;
+      text-align: left;
+      padding-top: 30px;
+      padding-bottom: 21px;
+      margin-top: 10px;
+    }
+  }
+`;
